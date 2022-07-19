97be4ad902f4b9c852f9a04d6dd59ce03445dec4
Akmatovedil <akmatov.ed@gmail.com>
2022-07-19 23:24:31 +0300

    Update extra.py

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,7 +1,7 @@
 import random
 
 from aiogram import types, Dispatcher
-from config import bot
+from config import bot, ADMIN
 
 async def echo(message: types.Message):
     bad = ['java', 'bitch', 'lox', 'kotlin', 'плохой мальчик']
@@ -11,10 +11,11 @@ async def echo(message: types.Message):
             await bot.delete_message(message.chat.id, message.message_id)
     if message.text.startswith('!pin') and message.reply_to_message:
         await bot.pin_chat_message(message.chat.id, message.message_id)
-    if message.text.startswith('game'):
+    if message.text.startswith('game') and message.from_user.id in ADMIN:
         arr = ['🎲', '😂', '🏆', '🧠' ]
         await bot.send_message(message.chat.id, random.choice(arr))
-
+    if message.text.startswith('game') and message.from_user.id not in ADMIN:
+        await bot.send_message(message.chat.id, f'Ты не админ {message.from_user.full_name}')
     # if message.text.lower() == 'dice':
     #     await bot.send_dice(message.chat.id, emoji=)
 

41bf1ef79bb7e245526ec8907ffffbb42fee4ad5
Akmatovedil <akmatov.ed@gmail.com>
2022-07-19 23:05:03 +0300

    Create extra.py

---
+++ handlers/extra.py
@@ -0,0 +1,23 @@
+import random
+
+from aiogram import types, Dispatcher
+from config import bot
+
+async def echo(message: types.Message):
+    bad = ['java', 'bitch', 'lox', 'kotlin', 'плохой мальчик']
+    for word in bad:
+        if word in message.text.replace(' ','').lower():
+            await bot.send_message(message.chat.id, f'Не материтесь {message.from_user.full_name}')
+            await bot.delete_message(message.chat.id, message.message_id)
+    if message.text.startswith('!pin') and message.reply_to_message:
+        await bot.pin_chat_message(message.chat.id, message.message_id)
+    if message.text.startswith('game'):
+        arr = ['🎲', '😂', '🏆', '🧠' ]
+        await bot.send_message(message.chat.id, random.choice(arr))
+
+    # if message.text.lower() == 'dice':
+    #     await bot.send_dice(message.chat.id, emoji=)
+
+def register_handlers_extra(dp: Dispatcher):
+    dp.register_message_handler(echo)
+
